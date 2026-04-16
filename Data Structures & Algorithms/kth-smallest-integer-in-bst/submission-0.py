8917e9e62b1ecbd36bcec95d9f57d1ea5301df0a
mariacfox <67665262+mariacfox@users.noreply.github.com>
2026-04-16 09:06:22 -0400

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # iterative DFS/stack approach
+        n = 0
+        stack = []
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            n += 1
+            if n == k:
+                return curr.val
+            curr = curr.right # loop goes back up 
