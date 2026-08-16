f4807fffa53188583fe0700175a5354d3e9bea68
Maria Fox <67665262+mariacfox@users.noreply.github.com>
2026-08-16 10:40:43 -0400

    Add: sql-grand-slam-titles - submission-1

---
+++ SQL/sql-grand-slam-titles/submission-1.sql
@@ -0,0 +1,15 @@
+SELECT
+    p.player_id,
+    p.player_name,
+    COUNT(*) AS grand_slams_count
+FROM players p
+JOIN (
+    SELECT wimbledon AS player_id FROM championships
+    UNION ALL
+    SELECT fr_open FROM championships
+    UNION ALL
+    SELECT us_open FROM championships
+    UNION ALL
+    SELECT au_open FROM championships
+) AS winners ON p.player_id = winners.player_id
+GROUP BY p.player_id, p.player_name;
