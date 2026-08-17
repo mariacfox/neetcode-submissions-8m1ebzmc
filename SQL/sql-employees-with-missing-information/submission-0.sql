8a446b5a21c50177b3fb10e20eb780585e2a1f71
Maria Fox <67665262+mariacfox@users.noreply.github.com>
2026-08-16 20:41:12 -0400

    Add: sql-employees-with-missing-information - submission-0

---
+++ SQL/sql-employees-with-missing-information/submission-0.sql
@@ -0,0 +1,8 @@
+select employee_id
+from (
+select coalesce(e.employee_id, s.employee_id) as employee_id,
+    name, salary
+from employees e
+full join salaries s on e.employee_id = s.employee_id
+) 
+where name is null or salary is null;
